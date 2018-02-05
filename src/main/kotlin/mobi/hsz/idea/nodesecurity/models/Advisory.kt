109fd3a730c405301d2077ead4538a96fa7fa610
hsz <jakub@chrzanowski.info>
2018-02-05 23:49:55 +0100

    Catch SemverException and return false if version is not valid

--- src/main/kotlin/mobi/hsz/idea/nodesecurity/models/Advisory.kt
+++ src/main/kotlin/mobi/hsz/idea/nodesecurity/models/Advisory.kt
@@ -4,6 +4,7 @@ import com.github.kittinunf.fuel.core.ResponseDeserializable
 import com.google.gson.Gson
 import com.intellij.openapi.util.text.StringUtil
 import com.vdurmont.semver4j.Semver
+import com.vdurmont.semver4j.SemverException
 
 data class Advisory(
         val id: Int = 0,
@@ -32,10 +33,14 @@ data class Advisory(
 //        override fun deserialize(content: String) = GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").create().fromJson(content, Advisory::class.java)
     }
 
-    fun isVulnerable(version: String): Boolean =
-            Semver(
-                    // TODO: get the real version of the installed package
-                    StringUtil.trimLeading(StringUtil.trimLeading(version, '~'), '^'),
-                    Semver.SemverType.NPM
-            ).satisfies(vulnerable_versions)
+    fun isVulnerable(version: String): Boolean = try {
+        Semver(
+                // TODO: get the real version of the installed package
+                StringUtil.trimLeading(StringUtil.trimLeading(version, '~'), '^'),
+                Semver.SemverType.NPM
+        ).satisfies(vulnerable_versions)
+    } catch (e: SemverException) {
+        false
+    }
+
 }
